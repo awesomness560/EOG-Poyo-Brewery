5dfd0848dd23cc1c638b2181ec03af09cf947165
YesMyNameIsMe <nguyen424787@gmail.com>
2025-11-09 07:54:29 -0600

    Prediction Animation

--- frontend/src/pages/history.tsx
+++ frontend/src/pages/history.tsx
@@ -3,10 +3,11 @@ import TopNavigation from '../Components/TopNavigation';
 
 const History: React.FC = () => {
   return (
-    <div className="flex justify-center bg-cover bg-center relative h-[3000px] w-full" 
-         style={{backgroundImage: "url('assets/prediction.svg')", overflow: 'hidden'}}>  
-      <TopNavigation />
+    <div className="flex justify-center bg-cover bg-center relative h-[1500px] w-full" 
+         style={{backgroundImage: "url('assets/HistoryBG.svg')", overflow: 'hidden'}}>  
+        <TopNavigation />
          
+         <img className="absolute left-35 bottom-70" src="/assets/PoyoCauldron.svg"/>
     </div>
   )
 }
--- frontend/src/pages/prediction.tsx
+++ frontend/src/pages/prediction.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import TopNavigation from '../Components/TopNavigation';
+import { AnimatePresence, delay, motion, useScroll } from 'framer-motion'
 
 const Predictions: React.FC = () => {
   return (
     <div className="flex justify-center bg-cover bg-center relative h-[3000px] w-full" 
-         style={{backgroundImage: "url('assets/prediction.svg')", overflow: 'hidden'}}>  
-      <TopNavigation />
+         style={{backgroundImage: "url('assets/PredictionBG.svg')", overflow: 'hidden'}}>  
          
+
+      <motion.div className="flex relative w-full h-full justify-center scale-90" 
+                  initial={{y:40, opacity: 0}} animate={{opacity: 1, y: 0}} transition={{duration: 0.6}}>
+        <img className="absolute" src="/assets/CrystalBall.svg"/>
+        <motion.img className="absolute " src="/assets/BallGoop.svg" initial={{rotate: 0}} animate={{rotate: 360}} transition={{repeat: Infinity, duration: 15, ease: 'linear'}} />
+      </motion.div>
+      <TopNavigation />
     </div>
   )
 }
