e5b3665ea9bebdebfce9266d25b352d40377f65b
Haris-Kadar <haris@convecap.com>
2025-11-09 10:57:32 -0600

    homepage

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,20 +1,54 @@
-import React from 'react';
+import React from "react";
+import TopNavigation from "../Components/TopNavigation";
 
-import TopNavigation from '../Components/TopNavigation';
-
-
-const Home:React.FC = () => {
-
+const Home: React.FC = () => {
   return (
-    <div className="flex justify-center bg-cover bg-center relative h-[3000px] w-full" 
-         style={{backgroundImage: "url('assets/HomeBG.svg')", overflow: 'hidden'}}> 
-         <TopNavigation /> 
-
-    </div>
+    <div style={{ position: "relative", width: "100vw", height: "100vh" }}>
     
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          zIndex: 100,
+        }}
+      >
+        <TopNavigation />
+      </div>
+
+      
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          overflow: "auto", 
+        }}
+      >
+        <img
+          src="/assets/HomeBG.svg"
+          alt="Background"
+          style={{
+            width: "150vw",   
+            height: "150vh",  
+            objectFit: "cover", 
+            display: "block",
+          }}
+        />
+        <img
+      src="/assets/Poyo.svg"
+      style={{
+          position: "fixed",
+          top: 780,
+          left: 80,
+          zIndex: 4,
+        }}
+      
+      ></img>
+      </div>
+    </div>
+
   );
 };
-
-
 
 export default Home;
